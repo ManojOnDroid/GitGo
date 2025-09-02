b1e7607c5771e7a09ad3de4a659f886ef600fd81
Manoj R Hemadri <manoj.hemadri@zopsmart.com>
2025-09-02 17:54:49 +0530

    inital setuop

---
+++ app/src/main/java/com/example/gitgo/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.gitgo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.gitgo.homeScreen.screen.HomeScreen
+import com.example.gitgo.searchScreen.screen.SearchScreen
+import com.example.gitgo.ui.theme.GitGoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GitGoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    GitHubExplorerApp(Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun GitHubExplorerApp(modifier: Modifier) {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") { HomeScreen(navController,modifier) }
+        composable("search") { SearchScreen(navController,modifier) }
+    }
+}
+
